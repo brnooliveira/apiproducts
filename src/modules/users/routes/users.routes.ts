9243f071a3cdb8bfd9d87bc323fcc703b64c7252
brnooliveira <brno.oliveiras@gmail.com>
2022-12-19 20:59:44 -0300

    add static route to files

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -2,10 +2,17 @@ import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import UsersController from '../controllers/UsersController';
 import isAuth from '../../../shared/http/middlewares/IsAuth';
+import multer from 'multer';
+import upload from '../../../config/upload';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
+
+const usersAvatarController = new UserAvatarController();
+
+const uploadMulter = multer(upload);
 
 usersRouter.get('/', isAuth, usersController.index);
 
@@ -19,6 +26,13 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+
+usersRouter.patch(
+  '/avatar',
+  isAuth,
+  uploadMulter.single('avatar'),
+  usersAvatarController.update,
 );
 
 export default usersRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,12 +6,15 @@ import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '../errors/AppError';
 import '../typeorm';
+import upload from '../../config/upload';
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use('/files', express.static(upload.directory));
 
 app.use(routes);
 
